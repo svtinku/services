adab96f385a58f94e7882dd028167c0e8d94e2ea
Raja <mssvtinku@outlook.com>
2019-02-05 14:42:34 +0530

    mailsend

--- RegisterService.php
+++ RegisterService.php
@@ -4,12 +4,13 @@ final class RegisterService extends Services
 {
 	//const KEYS = array("email", "phone", "pwd", "city", "name", "categ");
 	const KEYS = array(
-			"city"=>array("mandatory"=>false),
-			"name"=>array("mandatory"=>true),
-			"categ"=>array("mandatory"=>false),
-			"email"=>array("mandatory"=>true, "filter"=>FILTER_VALIDATE_EMAIL, "err"=>"Invalid Email","mode"=>"email" ), 
-			"phone"=>array("mandatory"=>true, "pattern"=>"/^[0-9]{10,12}$/", "err"=>"Invalid phone no.","mode"=>"phone" ),
-			"pwd"=>array("mandatory"=>true, "pattern"=>"/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/", "err"=>"Invalid password" )
+		"city"=>array("mandatory"=>false),
+		"name"=>array("mandatory"=>true),
+		"categ"=>array("mandatory"=>false),
+		"email"=>array("mandatory"=>true, "filter"=>FILTER_VALIDATE_EMAIL, "err"=>"Invalid Email","mode"=>"email" ), 
+		"phone"=>array("mandatory"=>true, "pattern"=>"/^[0-9]{10,12}$/", "err"=>"Invalid phone no.","mode"=>"phone" ),
+		"pwd"=>array("mandatory"=>true, "pattern"=>"/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/", "err"=>"Invalid password" )
+	
 	);
 	function get()
 	{
@@ -25,16 +26,26 @@ final class RegisterService extends Services
 		{
 			echo "Alrealy exists";
 			return $payload;
+		
 		}
 		else
-		{	$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*_";
-			$password=$payload->pwd = substr( str_shuffle( $chars ), 0, 4 );
+		{	
+			$to      = '$email';
+			$subject = 'the subject';
+			$message = '$password';
+			$headers = 'From: bizvoice.india@gmail.com' . "\r\n" .'Reply-To: $email' . "\r\n" .'X-Mailer: PHP/' . phpversion();
+
+			mail($to, $subject, $message, $headers);
+
+			$passcode = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*_";
+			$password=$payload->pwd = substr( str_shuffle( $passcode ), 0, 19 );
 			$stmt = $this->db->prepareQuery("insert into user(emailId,phone,name,city,category_id,password) values(?,?,?,?,?,?)");
 			$stmt->bind_param('ssssds', $payload->email, $payload->phone, $payload->name, $payload->city, $payload->categ, $payload->pwd);
 			$stmt->execute();
 			$stmt->close();
 			$this->db->commit();
 			return $payload;
+		
 		}
 	}
 }
